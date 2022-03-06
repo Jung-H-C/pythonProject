fbd0a99a1c98a94267ad7eb172d29bcc6f89c23f
Jung-H-C <xotourllife@naver.com>
2022-03-06 18:00:58 +0900

    220306 1800

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ def fahrenheit_to_cel(fahrenheit):
     return (fahrenheit - 32) * 5 / 9
 
 temperature_list = [40, 15, 32, 64, -4, 11]
+
+i = 0
+
+while i < len(temperature_list):
+    temperature_list[i] = round(fahrenheit_to_cel(temperature_list[i]), 1)
+    print(temperature_list[i])
+    i += 1
+
+print("why")
